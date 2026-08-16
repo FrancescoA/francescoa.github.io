37fc7f2c3ea7fa045c9f8081f3d8a4793561078c
Francesco <42341483+FrancescoA@users.noreply.github.com>
2026-08-17 00:57:20 +0200

    some tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-// @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.francescoarata.com',
+
   fonts: [{
     provider: fontProviders.fontsource(),
     name: "Inter",
     cssVariable: "--font-inter",
-  }]
+  }],
 
 });
