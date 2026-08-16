754692b019d631afd6d5b47d730ffe472d46dfa3
Francesco <42341483+FrancescoA@users.noreply.github.com>
2026-08-16 15:04:11 +0200

    first draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.francescoarata.com',
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Inter",
+    cssVariable: "--font-inter",
+  }]
+
 });
